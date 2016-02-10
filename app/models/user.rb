55cd8d6460c44a2129081510ea5d61b2bd34a82b
Rails Udemy Course <vishubaba186@gmail.com>
2016-02-10 20:42:23 +0000

    Add secure password and attribute to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,7 @@ class User < ActiveRecord::Base
              uniqueness: {case_sensitive: false},
              format: {with: VALID_EMAIL_REGEX}
              
+  has_secure_password           
+             
   
 end
